002182eaf5189f240e67070c53a50887f9008713
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-07-31 16:23:14 +0530

    implement uc1 code

---
+++ User1/User1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
